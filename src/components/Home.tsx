53338e8c8f3dbc2194bd7a3074420c4db68b95d9
bobzilla <heiariim1@gmail.com>
2024-12-09 14:30:57 -1000

    Home > delete motion path

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,26 @@
+import { motion } from "motion/react";
 import styled from "styled-components";
-import Blob from "./Blob";
+
 
 export const Home: React.FC = () => {
-    return (
-      <Homestyled>
-         <Blob />
-        <h1>Bienvenue sur mon portfolio</h1>
-        <h2>
-          Je suis Heiarii, développeur passionné par React et TypeScript.
-        </h2>
-      </Homestyled>
-    );
-  };
 
-  const Homestyled = styled.div`
+  
+    
+
+  return (
+    <Homestyled>
+          <h1>Bienvenue sur mon portfolio</h1>
+          <h2>
+            Je suis Heiarii, développeur passionné par <span>React et TypeScript.</span>
+          </h2>
+
+      
+
+    </Homestyled>
+  );
+};
+
+const Homestyled = styled.div`
    
   height: 100dvh;
   display: flex;
@@ -22,17 +29,30 @@ export const Home: React.FC = () => {
   align-items: center;
   background-color: #b3552c;
   text-align: center;
-  padding: 16px;
+  position: relative;
   gap: 80px;
   
-
-
+  div{
+    width: 100%;
+  }
+.bdf{
+  width: 150px;
+  height:  150px;
+  color: red;
+  position: absolute;
+  left: 40px;
+  top: 50px;
+  border-radius: 50%;
+  background-color: yellow;
+  
+}
  h1 {
   font-size: 6.5rem;
   color: #33efec;
+ 
 }
 
- p {
+ h2 {
   margin-top: 16px;
   font-size: 1.6rem;
   color: black;
