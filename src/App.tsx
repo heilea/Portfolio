56021e2f65f8228ec977a03023c035b99c78b52e
bobzilla <heiariim1@gmail.com>
2024-12-12 09:39:10 -1000

    delete import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Header } from "./components/header/Header";
 import { Home } from "./components/home/Home";
 import { Projects } from "./components/projects/Projects";
 import { Contact } from "./components/contact/Contact";
-import styled from "styled-components";
 
 
 const App: React.FC = () => {
